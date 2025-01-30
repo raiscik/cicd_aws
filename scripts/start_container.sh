cc6aedaee8fc87165ab0929f85b285f4f0c4cb86
raiscik <rais@ciklum.com>
2025-01-30 11:45:44 +0530

    made changes to start container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rachel2025/simple-python-flask-app:latest
+docker pull rachel2025/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rachel2025/simple-python-flask-app:latest
+docker run -d -p 5000:5000 rachel2025/simple-python-app:latest
